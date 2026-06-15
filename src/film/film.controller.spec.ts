495080700aad04b3860d24f17696b77bba2674ca
Zakwan <zakwan.adimasyqi@widyatama.ac.id>
2026-06-15 16:43:16 +0700

    fix: resolve NestJS dependency in tests

--- src/film/film.controller.spec.ts
+++ src/film/film.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmController } from './film.controller';
+import { FilmService } from './film.service';
 
 describe('FilmController', () => {
   let controller: FilmController;
 
+  const mockFilmService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilmController],
+      providers: [
+        {
+          provide: FilmService,
+          useValue: mockFilmService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FilmController>(FilmController);
--- src/film/film.service.spec.ts
+++ src/film/film.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmService } from './film.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('FilmService', () => {
   let service: FilmService;
 
+  const mockPrismaService = {
+    film: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilmService],
+      providers: [
+        FilmService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<FilmService>(FilmService);
