1a6afdd54b7ebe8fe0fea1e4107d7ac5df8a82a6
panduwibowo-eng <pandu.wibowo@widyama.ac.id>
2026-06-16 12:32:46 +0700

    update code dto create-pemesanan

--- src/pemesanan/dto/create-pemesanan.dto.ts
+++ src/pemesanan/dto/create-pemesanan.dto.ts
@@ -1,14 +1,24 @@
 import {
-    IsArray,
-    ArrayMinSize,
-    IsInt,
-  } from 'class-validator';
-  
-  export class CreatePemesananDto {
-    @IsInt()
-    id_jadwal: number;
-  
-    @IsArray()
-    @ArrayMinSize(1)
-    id_slots: number[];
-  }
+  IsInt,
+  IsArray,
+  ArrayMinSize,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePemesananDto {
+  @ApiProperty({
+    example: 1,
+  })
+  @Type(() => Number)
+  @IsInt()
+  id_jadwal: number;
+
+  @ApiProperty({
+    type: [Number],
+    example: [1, 2, 3],
+  })
+  @IsArray()
+  @ArrayMinSize(1)
+  id_slots: number[];
+}
